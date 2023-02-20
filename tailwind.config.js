e350efa34995bbe7471a602bdb2cd58913f0ea08
DevAntonioRogers <hydrohs6@gmail.com>
2023-02-19 21:58:10 -0500

    Few Bug Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      keyframes: {
-        wiggle: {
-          '0%, 100%': { transform: 'rotate(-3deg)' },
-          '50%': { transform: 'rotate(3deg)' },
-        }
-      },
-      animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite',
-      }
-    }
+    extend: {}
   }
 }
