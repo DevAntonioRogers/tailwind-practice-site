321bb6089c2e8dc59780ddbc1d8c00abb25a6e68
DevAntonioRogers <ar919061493@gmail.com>
2023-03-25 22:02:15 -0400

    Bug Fixes

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const navMenu = document.querySelector('.nav-menu')
-const icon = document.querySelector(".mobile-icon")
+const navMenu = document.querySelector('.nav-menu');
+const icon = document.querySelector('.mobile-icon');
+const links = document.querySelectorAll('.nav-menu a');
+const body = document.querySelector('body');
 
 icon.addEventListener('click', () => {
-  navMenu.classList.toggle('left-0')
-  icon.name = icon.name === 'menu' ? 'close' : 'menu'
-})
+  navMenu.classList.toggle('left-0');
+  icon.name = icon.name === 'menu' ? 'close' : 'menu';
+  body.classList.toggle('overflow-hidden');
+});
+
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('left-0');
+    icon.name = 'menu';
+    body.classList.remove('overflow-hidden');
+  });
+});
